f92c5d62556be00b3e1057347a9f528497c53a77
= <=>
2022-03-01 15:56:05 +0700

    update

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Create Component PrivatePage
+function PrivatePage(props) {
+  const { logout } = props
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={logout} >Logout</button>
+    </div>
+  )
+}
 
 //Create Component GuestPage
+function GuestPage(props) {
+  const { login } = props
+
+  return (
+    <div>
+      <h1>Please sign in</h1>
+      <button onClick={login} >login</button>
+    </div>
+  )
+}
 
 function ConditionRenderin() {
   //init State
+  const [isLogin, setIsLogin] = useState(false)
+
+  console.log(isLogin);
   return (
     // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>
+      {isLogin ? (
+        <PrivatePage logout={() => setIsLogin(!isLogin)} />
+      ) : (
+        <GuestPage login={() => setIsLogin(!isLogin)} />
+      )}
+
+    </div>
   );
 }
 
