03315614a3442dd3d1e8ecb11bdc86348df78eb0
= <=>
2022-03-01 15:27:59 +0700

    update

--- src/state.js
+++ src/state.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function State() {
   //init State
+  const [counter, setCounter] = useState(0)
+
+  console.log(counter);
 
   //Create Function Add
+  function Add() {
+    setCounter(counter + 8)
+  }
 
   //Create Function Less
+  function Less() {
+    setCounter(counter - 1)
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h2>{counter}</h2>
+      <button onClick={Add} >add</button>
+      <button onClick={Less} >less</button>
+    </div>
   );
 }
 
